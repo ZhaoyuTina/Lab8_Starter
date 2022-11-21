96c9f033fb496cd5e9d52bb44452c2b1f7e4c489
ZhaoyuTina <tinazzy2018@outlook.com>
2022-11-20 23:54:57 -0800

    Done

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Lab 8 - Starter
+
+1. Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+- Manually run them locally before pushing code 
+- So that we can prevnet crashing other codes.
+
+2. Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+- No. We should use Unit Testing to test function return output.
+
+3. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+- Yes I will. Because sending real messages might be time consuming and annoying. There are unit test for functions such as sending emails so I believe there exists similar functions for testing sending messages.
+
+4. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+- Yes. I think obtaining the message length and compare with 80 is simple and can be achieved with unit testing.
+
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,87 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+// matches valid phone numbers
+test('isPhoneNumber test 1', () => {
+    expect(functions.isPhoneNumber("110-101-2022")).toBe(true);
+});
+
+test('isPhoneNumber test 2', () => {
+    expect(functions.isPhoneNumber("100184-1092")).toBe(true);
+});
+
+test('isPhoneNumber test 3', () => {
+    expect(functions.isPhoneNumber("123abc3456")).toBe(false);
+});
+
+test('isPhoneNumber test 4', () => {
+    expect(functions.isPhoneNumber("11212239678")).toBe(false);
+});
+
+// matches valid emails
+test('isEmail test 1', () => {
+    expect(functions.isEmail("laowang@ucsd.edu")).toBe(true);
+});
+
+test('isEmail test 2', () => {
+    expect(functions.isEmail("spot@outlook.com")).toBe(true);
+});
+
+test('isEmail test 3', () => {
+    expect(functions.isEmail("abaaba")).toBe(false);
+});
+
+test('isEmail test 4', () => {
+    expect(functions.isEmail("loveCapoo@Cappoo.bugcat")).toBe(false);
+});
+
+// check strong password
+test('isStrongPassword test 1', () => {
+    expect(functions.isStrongPassword("catcapoo1234")).toBe(true);
+});
+
+test('isStrongPassword test 2', () => {
+    expect(functions.isStrongPassword("NengnaAforcse")).toBe(true);
+});
+
+test('isStrongPassword test 3', () => {
+    expect(functions.isStrongPassword("0biaji")).toBe(false);
+});
+
+test('isStrongPassword test 4', () => {
+    expect(functions.isStrongPassword("icecream~")).toBe(false);
+});
+
+// matches dates form
+test('isDate test 1', () => {
+    expect(functions.isDate("11/16/2022")).toBe(true);
+});
+
+test('isDate test 2', () => {
+    expect(functions.isDate("11/18/2022")).toBe(true);
+});
+
+test('isDate test 3', () => {
+    expect(functions.isDate("2001/11/2021")).toBe(false);
+});
+
+test('isDate test 4', () => {
+    expect(functions.isDate("tada10/21")).toBe(false);
+});
+
+// matches hex formax
+test('isHexColor test 1', () => {
+    expect(functions.isHexColor("E599F1")).toBe(true);
+});
+
+test('isHexColor test 2', () => {
+    expect(functions.isHexColor("b58")).toBe(true);
+});
+
+test('isHexColor test 3', () => {
+    expect(functions.isHexColor("88888518")).toBe(false);
+});
+
+test('isHexColor test 4', () => {
+    expect(functions.isHexColor("149149149")).toBe(false);
+});
